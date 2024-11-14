ffbd3aeb0993c832378242f364d77c87fa5dcce4
sam <sam.samarasooriya88@gmail.com>
2024-11-15 00:02:31 +1300

    minor change

--- models/userInterface.ts
+++ models/userInterface.ts
@@ -1,5 +1,21 @@
 export interface UserData {
-  id: number
-  user: string
-  proPicture: string
+  user: string;
+  profile_picture?: string;
+  bio?: string;
 }
+
+export interface User extends UserData{
+  id: number
+}
+
+export interface InsultData{
+  insult: string;
+  insultee_id: number;
+  insulter_id: number;
+  likes: number;
+  dislike: number;
+}
+export interface Insult extends InsultData {
+    id: number;
+
+  }
