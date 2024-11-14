c61de4a92fdd00b868fcbf723d6564d839efc1fe
sam <sam.samarasooriya88@gmail.com>
2024-11-14 23:40:53 +1300

    Migration and seed done

--- server/db/fruits.ts
+++ server/db/fruits.ts
@@ -1,5 +1,5 @@
 import db from './connection.ts'
-import { Fruit, FruitData } from '../../models/fruit.ts'
+import { Fruit, FruitData } from '../../models/userInterface.ts'
 
 export async function getAllFruits() {
   const fruit = await db('fruit').select()
