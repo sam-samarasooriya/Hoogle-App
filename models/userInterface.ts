6b99b773f1831acc8c2704658d33963ac61670d7
sam <sam.samarasooriya88@gmail.com>
2024-11-15 00:08:07 +1300

    created model interface'

--- models/userInterface.ts
+++ models/userInterface.ts
@@ -1,21 +1,20 @@
 export interface UserData {
-  user: string;
-  profile_picture?: string;
-  bio?: string;
+  user: string
+  profile_picture?: string
+  bio?: string
 }
 
-export interface User extends UserData{
+export interface User extends UserData {
   id: number
 }
 
-export interface InsultData{
-  insult: string;
-  insultee_id: number;
-  insulter_id: number;
-  likes: number;
-  dislike: number;
+export interface InsultData {
+  insult: string
+  insultee_id?: number
+  insulter_id?: number
+  likes?: number
+  dislike?: number
 }
 export interface Insult extends InsultData {
-    id: number;
-
-  }
+  id: number
+}
